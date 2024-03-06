7d6d5ba6d039ab5acccd6fdcc3f62ae198ad0f79
gabymg26 <gabrielamoralesgarcia2004@gmail.com>
2024-03-06 07:37:46 -0500

    Añadiendo la clase de configuración

---
+++ src/main/java/com/login/app/seguridad/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.login.app.seguridad;
+import com.login.app.servicio.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration{
+
+    @Autowired
+    private UsuarioServicio usuarioServicio;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(usuarioServicio);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
+
+    //Determina si los datos son válidos o no
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+}
--- src/main/java/com/login/app/servicio/UsuarioServicio.java
+++ src/main/java/com/login/app/servicio/UsuarioServicio.java
@@ -2,7 +2,9 @@ package com.login.app.servicio;
 
 import com.login.app.dto.UsuarioRegistroDTO;
 import com.login.app.modelo.Usuario;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public  interface UsuarioServicio{
+public  interface UsuarioServicio extends UserDetailsService {
     public Usuario guardarUsuario(UsuarioRegistroDTO registroDTO);
 }

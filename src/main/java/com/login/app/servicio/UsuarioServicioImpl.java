f3a4d3f6553c1d9c899767c22fdb485ec8ed4390
gabymg26 <gabrielamoralesgarcia2004@gmail.com>
2024-03-08 14:55:50 -0500

    Creando el archivo login.html

--- src/main/java/com/login/app/servicio/UsuarioServicioImpl.java
+++ src/main/java/com/login/app/servicio/UsuarioServicioImpl.java
@@ -4,14 +4,25 @@ import com.login.app.dto.UsuarioRegistroDTO;
 import com.login.app.modelo.Rol;
 import com.login.app.modelo.Usuario;
 import com.login.app.repositorio.UsuarioRepositorio;
+import org.hibernate.sql.ast.tree.expression.Collation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class UsuarioServicioImpl implements UsuarioServicio{
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     private UsuarioRepositorio usuarioRepositorio;
 
@@ -22,13 +33,21 @@ public class UsuarioServicioImpl implements UsuarioServicio{
     @Override
     public Usuario guardarUsuario(UsuarioRegistroDTO registroDTO) {
         Usuario usuario = new Usuario(registroDTO.getNombre(),
-                registroDTO.getApellido(), registroDTO.getEmail(), registroDTO.getPassword(),
+                registroDTO.getApellido(), registroDTO.getEmail(), passwordEncoder.encode(registroDTO.getPassword()),
                 Arrays.asList(new Rol("ROLE_USER")));
         return usuarioRepositorio.save(usuario);
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        Usuario usuario = usuarioRepositorio.findByEmail(username);
+        if (usuario == null){
+            throw new UsernameNotFoundException("Usuario o password inválido");
+        }
+        return new User(usuario.getEmail(), usuario.getPassword() , mapearAutoridadesRoles(usuario.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapearAutoridadesRoles(Collection<Rol> roles){
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getNombre())).collect(Collectors.toList());
     }
 }

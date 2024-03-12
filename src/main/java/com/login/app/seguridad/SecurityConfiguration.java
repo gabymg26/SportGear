fae71a59c8d5dfb24dc170cff83a72ce856dd0ac
gabymg26 <gabrielamoralesgarcia2004@gmail.com>
2024-03-11 22:45:46 -0500

    Modificando el login

--- src/main/java/com/login/app/seguridad/SecurityConfiguration.java
+++ src/main/java/com/login/app/seguridad/SecurityConfiguration.java
@@ -1,17 +1,17 @@
 package com.login.app.seguridad;
-import com.login.app.servicio.UsuarioServicio;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-
+import com.login.app.servicio.UsuarioServicio;
 
 @Configuration
 @EnableWebSecurity
@@ -21,27 +21,41 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     private UsuarioServicio usuarioServicio;
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
+    public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
         auth.setUserDetailsService(usuarioServicio);
         auth.setPasswordEncoder(passwordEncoder());
         return auth;
     }
 
-    //Determina si los datos son válidos o no
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.authorizeRequests().antMatchers("/registro**", "/js**/", "/css**/", "/img**").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permiteAll().and().logout().invalidateHttpSession(true).clearAuthentication;
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(
+                        "/registro**",
+                        "/js/**",
+                        "/css/**",
+                        "/img/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
     }
-
 }
